6080d324ce61a656935c70d4704ca79d501c1dc7
SeungMin <k0102575@gmail.com>
2020-05-18 00:45:06 +0900

    get json

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,23 @@
 <script>
   import Youtube from "./Youtube.svelte";
   import { store } from "./stores.js";
+  import { onMount, onDestroy, beforeUpdate, afterUpdate, tick } from "svelte";
   let player;
   let videoIds = ["-sVo6NWwK_o", "hRJKGVJpHpA", "7gr7BK7B51g"];
   let idx;
   store.subscribe(i => (idx = i));
+
+  onMount(() => {
+    var urly = "/test.json";
+    fetch(urly)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+      });
+  });
 </script>
 
-<button
+<!--<button
   on:click={() => {
     player.createPlayer(videoIds[idx]);
   }}>
@@ -24,6 +34,6 @@
     player.destroyPlayer();
   }}>
   삭제
-</button>
+</button>-->
 
 <Youtube bind:this={player} />
